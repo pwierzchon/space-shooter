490156b0f87d061a88624227ea7ac2480f9e16b8
pwierzchon <destino.fordon@gmail.com>
2017-10-03 15:48:28 +0200

    Core of the game with hazards and scoring.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	public GameController gameController;
+
+	void Start(){
+		GameObject gcObject = GameObject.FindWithTag ("GameController");	
+		if (gcObject != null) {
+			gameController = gcObject.GetComponent<GameController> ();
+		} else {
+			Debug.Log ("cannot find GameController instance");
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag != "Boundary") {
+			GameObject animation1 = Instantiate (explosion, transform.position, transform.rotation) as GameObject;
+
+			if(other.tag == "Player"){
+				GameObject animation2 = Instantiate (playerExplosion, other.transform.position, other.transform.rotation) as GameObject;
+			}
+			if (other.tag == "PlayerBolt") {
+				gameController.AddScore (scoreValue);
+			}
+
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public Text scoreText;
+	public int score;
+
+	void Start(){
+		score = 0;
+		UpdateScore ();
+		StartCoroutine(SpawnWaves ());
+
+	}
+		
+	IEnumerator SpawnWaves(){
+		yield return new WaitForSeconds (startWait);
+		while(true){
+    		for(int i = 0; i < hazardCount;i++){
+	    	    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x,spawnValues.x),0.0f, spawnValues.z);
+		        Quaternion spawnRotation = Quaternion.identity;
+
+         		GameObject hazardClone = Instantiate (hazard, spawnPosition, spawnRotation) as GameObject;
+	    		yield return new WaitForSeconds (spawnWait);
+		    }
+		}
+	}
+
+	public void AddScore(int scoreValue){
+		score += scoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore(){
+		scoreText.text = "Score: " + score.ToString ();
+	}
+
+}
